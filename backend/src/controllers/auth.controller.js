6d0faeaf6278fad8dabb32bfc3c35b056894dbee
Dacosmicgiant <vedantvanpro@gmail.com>
2025-01-16 12:14:03 +0530

    created message model

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -112,9 +112,9 @@ export const updateProfile = async (req,res) => {
 
 export const checkAuth = (req,res) => {
   try {
-    res.staus(200).json(req.user);
+    res.status(200).json(req.user);
   } catch (error) {
     console.log("Error in checkAuth contoller", error.message);
     res.status(500).json({ message: "Internal Server Error"});
   }
-}
+} //implemented
