4cee6b4ce5cd97078187b14e70efcd85a31d7a50
Dacosmicgiant <vedantvanpro@gmail.com>
2025-01-16 11:06:39 +0530

    initialized check route to check if user is logged in on refresh

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -109,3 +109,12 @@ export const updateProfile = async (req,res) => {
     res.status(500).json({message: "Internal server error"});
   }
 }; //implemented
+
+export const checkAuth = (req,res) => {
+  try {
+    res.staus(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth contoller", error.message);
+    res.status(500).json({ message: "Internal Server Error"});
+  }
+}
