aa0ac1a69219d1e0c8d2a35997c47f55a9153c61
Dacosmicgiant <vedantvanpro@gmail.com>
2025-01-16 13:30:23 +0530

    implemented getMessages function to get message history with respective user

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import Message from "../models/message.model.js"
 export const getUsersForSidebar = async (req, res) => {
     try{
         const loggedInUserId = req.user._id;
@@ -10,4 +10,22 @@ export const getUsersForSidebar = async (req, res) => {
         console.error("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({error: "Internal server error"});
     }
-};
+}; //implemented
+
+export const getMessages = async(req, res) => {
+    try{
+        const { id:userToChatId } =req.params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId:myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId}
+            ]
+        })
+        res.status(200).json(messages);
+    } catch (error){
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}; //implemented
